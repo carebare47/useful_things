18df8b7938bf33740f03aa7663f5f9007eb84042
Tom Queen <tom_q@hotmail.co.uk>
2019-02-06 10:33:01 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,8 +28,8 @@ else
     echo "no"
 fi
 
-sudo add-apt-repository ppa:gnome-terminator
-sudo apt-get update
+#sudo add-apt-repository ppa:gnome-terminator
+#sudo apt-get update
 sudo apt-get install -y gedit nano git curl terminator xsel jq
 #git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
 #echo "copy() { \"\$1\" | tr -d '\n' | xsel -ib ; }" >> ~/.bashrc
