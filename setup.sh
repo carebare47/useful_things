06c4ca3df0dddbc700de92dcb95a46500c43804b
Tom Queen <tom_q@hotmail.co.uk>
2019-02-06 10:26:24 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y gedit nano git 
+sudo apt-get install -y gedit nano git curl
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
 echo "copy() { \"\$1\" | tr -d '\n' | xsel -ib ; }" >> ~/.bashrc
 source ~/.bashrc
