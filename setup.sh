8710c653023625a736603e94081dba1908326158
Tom Queen <tom_q@hotmail.co.uk>
2019-02-06 10:27:23 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
-sudo apt-get install -y gedit nano git curl
+sudo add-apt-repository ppa:gnome-terminator
+sudo apt-get update
+sudo apt-get install -y gedit nano git curl terminator
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
 echo "copy() { \"\$1\" | tr -d '\n' | xsel -ib ; }" >> ~/.bashrc
 source ~/.bashrc
