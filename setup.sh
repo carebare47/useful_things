7d1ad3b2f9d8076f2b8942d2e3b8123ed19f0ffc
Tom Queen <tom_q@hotmail.co.uk>
2019-02-06 10:32:10 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,38 @@
+
+while [[ $# > 1 ]]
+do
+key="$1"
+
+case $key in
+    -f|--install_fzf)
+    FZF="$2"
+    shift
+    ;;
+    *)
+    # ignore unknown option
+    ;;
+esac
+shift
+done
+
+if [ -z "${FZF}" ];
+then
+    FZF=false
+else 
+    FZF=true
+fi
+
+if [ ${FZF} = true ]; then
+    echo "yes"
+else 
+    echo "no"
+fi
+
 sudo add-apt-repository ppa:gnome-terminator
 sudo apt-get update
-sudo apt-get install -y gedit nano git curl terminator
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
+sudo apt-get install -y gedit nano git curl terminator xsel jq
+#git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
 echo "copy() { \"\$1\" | tr -d '\n' | xsel -ib ; }" >> ~/.bashrc
+echo "list_dex() {curl 10.6.10.7:5000/v2/dexterous-hand/tags/list | jq -r ; }" >> ~/.bashrc
+echo "list_flex() {curl 10.6.10.7:5000/v2/flexible-hand/tags/list | jq -r ; }" >> ~/.bashrc
 source ~/.bashrc
