c2e3a06eb0f749cc63be09c423b71e523d9bbee6
Tom Queen <tom_q@hotmail.co.uk>
2019-02-06 10:24:05 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,3 @@
+sudo apt-get install -y gedit nano git 
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
+echo "copy() { \"\$1\" | tr -d '\n' | xsel -ib ; }" >> ~/.bashrc
