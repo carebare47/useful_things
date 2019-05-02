cc045e98081e9f8099c91100346f06ca8fb9a779
carebare47 <tom@shadowrobot.com>
2019-05-02 12:14:51 +0100

    adding nmap

--- setup.sh
+++ setup.sh
@@ -164,7 +164,7 @@ fi
 if [[ "${INSTALL_PACKAGES}" == true  ]]; then
 	echo "Installing packages"
 	sudo apt-get update
-	sudo apt-get install -y gedit nano git curl xsel jq tree	
+	sudo apt-get install -y gedit nano git curl xsel jq tree nmap
 fi
 
 if [[ "${INSTALL_FZF}" == true  ]]; then
