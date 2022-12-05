20cae572b64c90c85788f3e98b1f5ae9b0dca1e1
Tom Queen <tom_q@hotmail.co.uk>
2022-12-05 17:48:45 +0000

    Create install_docker.sh

---
+++ docker_ml/install_docker.sh
@@ -0,0 +1,16 @@
+export aurora_folder="$HOME/useful_things"
+
+sudo apt-get update
+
+sudo apt-get install -y python3-dev libxml2-dev libxslt-dev python3-pip lcov wget git libssl-dev libffi-dev libyaml-dev
+sudo pip3 install --upgrade pip setuptools==51.1.1 gcovr
+sudo pip3 install PyYAML==5.4.1 --ignore-installed
+
+# Clean up useful_things
+rm -rf $aurora_folder
+git clone --depth 1 git@github.com:shadow-robot/aurora/
+cd $aurora_folder/ansible
+
+sudo pip3 install -r data/requirements.txt
+
+PYTHONUNBUFFERED=1 ansible-playbook -v -i "localhost," -c local docker_site.yml --tags "installation/docker"
