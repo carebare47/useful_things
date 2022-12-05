fd9f35818bb8d04442c49fc613166d8820471fb9
Tom Queen <tom_q@hotmail.co.uk>
2022-12-05 18:16:52 +0000

    Update create_user.sh

--- docker_ml/create_user.sh
+++ docker_ml/create_user.sh
@@ -10,7 +10,7 @@ useradd -m $USERNAME
 echo "$USERNAME:$USER_PASSWORD" | chpasswd
 usermod --shell /bin/bash $USERNAME
 usermod -aG sudo $USERNAME
-usermod -aG systemd-journal $USERNAME
+# usermod -aG systemd-journal $USERNAME
 usermod -aG video $USERNAME
 echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/$USERNAME
 chmod 0440 /etc/sudoers.d/$USERNAME
